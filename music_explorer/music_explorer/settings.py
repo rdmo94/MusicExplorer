03060092e516d38a46a8fc06791961b811f07722
seanwachs <wachs@live.dk>
2022-05-16 23:29:46 +0200

    static files

--- music_explorer/music_explorer/settings.py
+++ music_explorer/music_explorer/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    #'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'rest_framework',
     'frontend.apps.FrontendConfig',
@@ -119,7 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-#STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
