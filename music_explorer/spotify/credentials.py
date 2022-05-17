7e6b4c980a7204bcd7607204413bde8c5ddbbcdf
seanwachs <wachs@live.dk>
2022-05-17 11:32:30 +0200

    changed redirect uri to server IP

--- music_explorer/spotify/credentials.py
+++ music_explorer/spotify/credentials.py
@@ -1,3 +1,3 @@
 CLIENT_ID = "97246a4390bf4516b9177ae13269fe86"
 CLIENT_SECRET = "b0546fc6e15e4db4ab491455c724dd19"
-REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
+REDIRECT_URI = "http://musicexplorer.dk:8000/spotify/redirect"
