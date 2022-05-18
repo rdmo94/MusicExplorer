529d7c5b37135b1d167b95348f77d6a0800a186e
seanwachs <wachs@live.dk>
2022-05-18 13:32:28 +0200

    graph changes

--- music_explorer/frontend/src/components/Graph2D.js
+++ music_explorer/frontend/src/components/Graph2D.js
@@ -11,45 +11,60 @@ import {
   ForceGraphAR,
   GraphData,
 } from "react-force-graph";
+import { styled, CircularProgress, LinearProgress, Slider, Box, Typography } from "@mui/material";
 
 /**
- * 
- * @param {object} param 
+ *
+ * @param {object} param
  * @param {object} param.data
  * @param {object} param.properties graph properties
  * @param {Map<String,number>} param.userGenreMap Map<genre,occurrence>
  * @param {Map<number,list<String>>} param.strategy Map<strategy_number,list<genres>>
  * @param {List<String>} param.links List<genres>
- * @returns 
+ * @returns
  */
-function Graph2D({ data, properties, userGenreMap, strategy_genres, height, width, nodeClickCallback, selectViewMode, graphIsLoadedCallback}) {
+function Graph2D({
+  data,
+  properties,
+  userGenreMap,
+  strategy_genres,
+  height,
+  width,
+  nodeClickCallback,
+  selectViewMode
+}) {
+  const [graphLoaded, setGraphLoaded] = useState(false);
 
-
-  function getNodeVisibility(node){
-    const min_node_weight = properties.genrePopularity
+  function getNodeVisibility(node) {
+    const min_node_weight = properties.genrePopularity;
     if (strategy_genres.includes(node.name)) return true;
-    if (node.weight < min_node_weight) return false;
-    else if (selectViewMode == "source"){
-      if (!(node.name in userGenreMap)){
-        return false;
-      }
-    } else if (selectViewMode == "target"){
-        if (node.name in userGenreMap){
-          return false;
-        }
-      } else {return true}
-    } 
 
+    if (node.weight < min_node_weight) return false;
+    else if (selectViewMode == "source") {
+      if (!(node.name in userGenreMap)) {
+        return false;
+      } else {
+        return true;
+      }
+    } else if (selectViewMode == "target") {
+      if (node.name in userGenreMap) {
+        return false;
+      } else {
+        return true;
+      }
+    } else {
+      return true;
+    }
+  }
 
   function getNodeVal(node, globalScale) {
-    const base_size = 10/globalScale;
-    const max_size = base_size+(10/globalScale)//4
-    const min_size = base_size//1
-    
-    const occurrence_divider = 8
-    const weight_divider = 100
-    const strategy_size = base_size+(10/globalScale)//6
+    const base_size = 10 / globalScale;
+    const max_size = base_size + 10 / globalScale; //4
+    const min_size = base_size; //1
 
+    const occurrence_divider = 8;
+    const weight_divider = 100;
+    const strategy_size = base_size + 10 / globalScale; //6
 
     if (userGenreMap) {
       if (strategy_genres && strategy_genres.includes(node.name)) {
@@ -61,11 +76,11 @@ function Graph2D({ data, properties, userGenreMap, strategy_genres, height, widt
         else return min_size;
       } else {
         //determine size based on weight
-        if (node.weight/weight_divider > min_size){
-          if (node.weight/weight_divider > max_size){
-            return max_size
+        if (node.weight / weight_divider > min_size) {
+          if (node.weight / weight_divider > max_size) {
+            return max_size;
           } else {
-            return node.weight/weight_divider;
+            return node.weight / weight_divider;
           }
         } else {
           return min_size;
@@ -77,84 +92,122 @@ function Graph2D({ data, properties, userGenreMap, strategy_genres, height, widt
   }
 
   function getNodeLabel(node) {
-    return (
-      replace_special_characters(node.name, false)//+
-      //  " [" +
-      //  getNodeVal(node, globalScale).toString() +
-      //  "]"
-    ); //TODO remove last part - only for testing
+    return replace_special_characters(node.name, false); //+
+    //  " [" +
+    //  getNodeVal(node, globalScale).toString() +
+    //  "]" //TODO remove last part - only for testing
   }
 
   function getNodeColor(node) {
     if (strategy_genres && strategy_genres.includes(node.name)) {
       //console.log(node.name + " in " + strategy_genres)
-      return '#0258ad'; //blue
+      return "#0258ad"; //blue
     } else if (userGenreMap && node.name in userGenreMap) {
-      return '#057a01'; //green
-    } 
-    else {
-      return '#5c5c5c'; //grey
+      return "#057a01"; //green
+    } else {
+      return "#5c5c5c"; //grey
     }
   }
 
   function getNodeBackgroundFillStyle(node) {
     if (strategy_genres && strategy_genres.includes(node.name)) {
-      return 'rgba(255, 255, 255, 0.8)'; //darker grey
+      return "rgba(255, 255, 255, 0.8)"; //darker grey
     } else {
-      return 'rgba(255, 255, 255, 0.1)'; //light grey
-    } 
+      return "rgba(255, 255, 255, 0.1)"; //light grey
+    }
   }
 
-  
+  function nodeClickCallbackFix(node){
+    if (selectViewMode == "source" || selectViewMode == "target") {
+      nodeClickCallback(node)
+    }
+  }
+
   return (
-    <ForceGraph2D
-      height={height}
-      width={width}
-      backgroundColor={'white'}//{primaryGrey}
-      enableNodeDrag={false}
-      onNodeClick={nodeClickCallback}
-      graphData={data}
-      onEngineStop={() => {
-        graphIsLoadedCallback(true)
-      }}
-      //nodeAutoColorBy={node => node.name in userGenreMap}
-      nodeVisibility={(node) => getNodeVisibility(node)}
-      nodeColor={(node) => getNodeColor(node)}
-      nodeLabel={(node) => getNodeLabel(node)} //label when hovering
-      //nodeVal={(node) => getNodeVal(node)}
-      
-      //nodeVal={node => node.name in userGenreMap ? userGenreMap[node.name].value : 1}
-      //nodeVal={100}
-      nodeCanvasObject={(node, ctx, globalScale) => {
-        const label = getNodeLabel(node);
-        // const fontSize = (getNodeVal(node) * 12) / globalScale; //higher is smaller?
-        const fontSize = (getNodeVal(node, globalScale)); //higher is smaller?
-        ctx.font = `${fontSize}px Sans-Serif`;
-        const textWidth = ctx.measureText(label).width;
-        const bckgDimensions = [textWidth, fontSize].map(
-          (n) => n + fontSize * 0.2
-        ); // some padding
+    <div>
+      <ForceGraph2D
+        height={height}
+        width={width}
+        backgroundColor={"white"} //{primaryGrey}
+        enableNodeDrag={false}
+        onNodeClick={nodeClickCallbackFix}
+        graphData={data}
+        onEngineStop={() => {
+          setGraphLoaded(true)
+        }}
+        //nodeAutoColorBy={node => node.name in userGenreMap}
+        nodeVisibility={(node) => getNodeVisibility(node)}
+        nodeColor={(node) => getNodeColor(node)}
+        nodeLabel={(node) => getNodeLabel(node)} //label when hovering
+        //nodeVal={(node) => getNodeVal(node)}
 
-        ctx.textAlign = "center";
-        ctx.textBaseline = "middle";
-        ctx.fillStyle = getNodeBackgroundFillStyle(node);
-        ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
-        ctx.fillStyle = getNodeColor(node);
-        ctx.fillText(label, node.x, node.y);
+        //nodeVal={node => node.name in userGenreMap ? userGenreMap[node.name].value : 1}
+        //nodeVal={100}
+        nodeCanvasObject={(node, ctx, globalScale) => {
+          const label = getNodeLabel(node);
+          // const fontSize = (getNodeVal(node) * 12) / globalScale; //higher is smaller?
+          const fontSize = getNodeVal(node, globalScale); //higher is smaller?
+          ctx.font = `${fontSize}px Sans-Serif`;
+          const textWidth = ctx.measureText(label).width;
+          const bckgDimensions = [textWidth, fontSize].map(
+            (n) => n + fontSize * 0.2
+          ); // some padding
 
-        node.__bckgDimensions = bckgDimensions; // to re-use in nodePointerAreaPaint
-      }}
-      nodePointerAreaPaint={(node, color, ctx) => {
-        ctx.fillStyle = color;
-        const bckgDimensions = node.__bckgDimensions;
-        bckgDimensions && ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
-      }}
-      minZoom={2}
-      //zoom={0.2} //doesnt work.. zz
-      linkColor={() => '#0258ad'}
-      linkOpacity={0.9}
-      linkWidth={1}
-    />
+          ctx.textAlign = "center";
+          ctx.textBaseline = "middle";
+          ctx.fillStyle = getNodeBackgroundFillStyle(node);
+          ctx.fillRect(
+            node.x - bckgDimensions[0] / 2,
+            node.y - bckgDimensions[1] / 2,
+            ...bckgDimensions
+          );
+          ctx.fillStyle = getNodeColor(node);
+          ctx.fillText(label, node.x, node.y);
+
+          node.__bckgDimensions = bckgDimensions; // to re-use in nodePointerAreaPaint
+        }}
+        nodePointerAreaPaint={(node, color, ctx) => {
+          ctx.fillStyle = color;
+          const bckgDimensions = node.__bckgDimensions;
+          bckgDimensions &&
+            ctx.fillRect(
+              node.x - bckgDimensions[0] / 2,
+              node.y - bckgDimensions[1] / 2,
+              ...bckgDimensions
+            );
+        }}
+        minZoom={2}
+        //zoom={0.2} //doesnt work.. zz
+        linkColor={() => "#ccc900"}
+        linkOpacity={0.9}
+        linkWidth={10}
+      />
+        {graphLoaded ? (
+          ""
+        ) : (
+          <Box
+            backgroundColor={"rgba(0, 0, 0, 0.4)"}
+            borderRadius={200}
+            position={"fixed"}
+            flexDirection={"column"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            flexGrow={3}
+            paddingTop={1}
+            paddingLeft={3}
+            paddingRight={3}
+            paddingBottom={1}
+            bottom={30}
+          >
+            <div>
+              <Typography style={{ color: "white" }}>
+                Graph is loading...
+              </Typography>
+              <LinearProgress />
+            </div>
+          </Box>
+        )}
+    </div>
   );
 }
 
--- music_explorer/frontend/src/components/Graph3D.js
+++ music_explorer/frontend/src/components/Graph3D.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {useState} from "react";
 import SpriteText from "three-spritetext";
 import { graph_data_prettyfier, replace_special_characters } from "../Util";
 
@@ -9,6 +10,15 @@ import {
   ForceGraphAR,
   GraphData,
 } from "react-force-graph";
+import {
+  styled,
+  CircularProgress,
+  LinearProgress,
+  Slider,
+  Box,
+  Typography,
+} from "@mui/material";
+import { StrikethroughS } from "@mui/icons-material";
 
 function Graph3D({
   data,
@@ -19,32 +29,38 @@ function Graph3D({
   width,
   nodeClickCallback,
   selectViewMode,
-  graphIsLoadedCallback
+  graphIsLoadedCallback,
 }) {
-
+  const [graphLoaded, setGraphLoaded] = useState(false);
 
   function getNodeVisibility(node) {
-    const min_node_weight = 100; //CONFIG
-    if (selectViewMode == "source") {
+    const min_node_weight = properties.genrePopularity;
+    if (strategy_genres.includes(node.name)) return true;
+
+    if (node.weight < min_node_weight) return false;
+    else if (selectViewMode == "source") {
       if (!(node.name in userGenreMap)) {
         return false;
+      } else {
+        return true;
       }
     } else if (selectViewMode == "target") {
       if (node.name in userGenreMap) {
         return false;
+      } else {
+        return true;
       }
+    } else {
+      return true;
     }
-    if (strategy_genres.includes(node.name)) return true;
-    if (node.weight > min_node_weight) return true;
-    else return false;
   }
 
   function getNodeVal(node) {
-    const occurrence_divider = 8;
+    const occurrence_divider = 2;
     const weight_divider = 100;
-    const max_size = 1.0;
-    const min_size = 0.5;
-    const strategy_size = max_size+3;
+    const max_size = 0.2;
+    const min_size = 0.1;
+    const strategy_size = max_size + 0.2;
 
     if (userGenreMap) {
       if (strategy_genres && strategy_genres.includes(node.name)) {
@@ -56,8 +72,8 @@ function Graph3D({
         else return min_size;
       } else {
         //determine size based on weight
-        if (node.weight / weight_divider > min_size) {
-          if (node.weight / weight_divider > max_size) {
+        if ((node.weight / weight_divider) > min_size) {
+          if ((node.weight / weight_divider) > max_size) {
             return max_size;
           } else {
             return node.weight / weight_divider;
@@ -72,58 +88,92 @@ function Graph3D({
   }
 
   function getNodeLabel(node) {
-    return (
-      replace_special_characters(node.name, false)//+
-      //  " [" +
-      //  getNodeVal(node, globalScale).toString() +
-      //  "]"
-    ); //TODO remove last part - only for testing
+    return replace_special_characters(node.name, false); //+
+    //  " [" +
+    //  getNodeVal(node, globalScale).toString() +
+    //  "]" //TODO remove last part - only for testing
   }
 
   function getNodeColor(node) {
     if (strategy_genres && strategy_genres.includes(node.name)) {
       //console.log(node.name + " in " + strategy_genres)
-      return '#0258ad'; //blue
+      return "#0258ad"; //blue
     } else if (userGenreMap && node.name in userGenreMap) {
-      return '#057a01'; //green
-    } 
-    else {
-      return '#5c5c5c'; //grey
+      return "#057a01"; //green
+    } else {
+      return "#5c5c5c"; //grey
     }
+  }
+
+  function nodeClickCallbackFix(node){
+    if (selectViewMode == "source" || selectViewMode == "target") {
+      nodeClickCallback(node)
+    } 
   }
 
   const { useRef } = React;
   const fgRef = useRef();
 
   return (
-    <ForceGraph3D
-      ref={fgRef}
-      onEngineStop={() => {
-        fgRef.current.zoomToFit(20,5, (n)=>true)
-        graphIsLoadedCallback(true)
-      }}
-      height={height}
-      width={width}
-      backgroundColor={properties.backgroundColor}
-      graphData={data}
-      onNodeClick={nodeClickCallback}
-      nodeThreeObject={(node) => {
-        const sprite = new SpriteText(getNodeLabel(node));
-        sprite.color = getNodeColor(node);
-        sprite.textHeight = getNodeVal(node)//0.5;
-        return sprite;
-      }}
-      enableNodeDrag={false}
-      nodeVisibility={(node) => getNodeVisibility(node)}
-      nodeLabel={(node) => getNodeLabel(node)} //label when hovering
-      nodeVal={(node) => getNodeVal(node)}
-      //zoom={0.2} //doesnt work.. zz
-      linkColor={() => '#0258ad'}
-      //zoomToFit={(1, 700, (_node) => true)}
-      linkOpacity={0.9}
-      linkWidth={0.2}
-      
-    />
+    <div>
+      <ForceGraph3D
+        ref={fgRef}
+        onEngineStop={() => {
+          fgRef.current.zoomToFit(400)
+          setGraphLoaded(true)
+        }}
+        height={height}
+        width={width}
+        backgroundColor={properties.backgroundColor}
+        graphData={data}
+        onNodeClick={nodeClickCallbackFix}
+        nodeThreeObject={(node) => {
+          const sprite = new SpriteText(getNodeLabel(node));
+          sprite.color = getNodeColor(node);
+          sprite.textHeight = getNodeVal(node);
+          sprite.strokeWidth = 1;
+          sprite.strokeColor = "lightgrey";
+          //sprite.borderWidth = 0.01
+          //sprite.backgroundColor = 'rgba(0,0,0,0.1)';
+          
+          return sprite;
+        }}
+        enableNodeDrag={false}
+        nodeVisibility={(node) => getNodeVisibility(node)}
+        nodeLabel={(node) => getNodeLabel(node)} //label when hovering
+        nodeVal={(node) => getNodeVal(node)}
+        //zoom={0.2} //doesnt work.. zz
+        linkColor={() => "#ccc900"}
+        //zoomToFit={(1, 700, (_node) => true)}
+        linkOpacity={0.9}
+        linkWidth={0.2}
+      />
+      {graphLoaded ? (
+        ""
+      ) : (
+        <Box
+          backgroundColor={"rgba(0, 0, 0, 0.4)"}
+          borderRadius={200}
+          position={"fixed"}
+          flexDirection={"column"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          flexGrow={3}
+          paddingTop={1}
+          paddingLeft={3}
+          paddingRight={3}
+          paddingBottom={1}
+          bottom={30}
+        >
+          <div>
+            <Typography style={{ color: "white" }}>
+              Graph is loading...
+            </Typography>
+            <LinearProgress />
+          </div>
+        </Box>
+      )}
+    </div>
   );
 }
 
