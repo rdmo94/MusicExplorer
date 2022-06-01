339bb8959f226452c7882265d37a8fdbdb6f8513
rdmo94 <rdmo94@gmail.com>
2022-06-01 06:42:27 +0200

    Small changes

--- README.md
+++ README.md
@@ -1,27 +1,43 @@
 # MusicExplorer
 Master Thesis project 2022
 
-# create new virtualenv
-pip3 install virtualenv
-virtualenv "name" -p python3
+# Folder contents
 
-# activate venv
-source venv/bin/activate
-deactivate
+## data_handling
+The folder containing python code used to collect and proccess data, create co-occurrence matrix, train a word2vec model and using TSNE and PCA to reduce dimensionality.
 
+file_generator.py is a script running all code necessary for producing the needed files used in MusicXplorer.
 
-# migrations
-python manage.py makemigrations
-python manage.py migrate
+### data
+This folder contains files produced and used in the flow of creating the word2vec model and TSNE, PCA and co-occurrence matrix.
+## music_explorer
+The application folder.
 
-# npm
-npm init -y
-npm i webpack webpack-cli --save-dev
-npm i react react-dom --save-dev
-npm install @material-ui/core
-npm install react-router-dom
-npm install @material-ui/icons
+### api
+The application folder for the API handling platform specific requests.
 
-npm i @babel/core babel-loader @babel/preset-env @babel/preset-react --save-dev
+#### data
+This folder contains statis data used for computations made in this API. 
+- A file fontaining all track ids of tracks listen on the playlists for each genre on Spotify.
+- A file containing the underlying network graph for findings a path.
+- A file containing the word2vec model.
 
-sudo npm i @babel/plugin-proposal-class-properties
+### spotify
+The application folder for the API handling Spotify specific requests.
+
+#### static data
+- A file containing the 2D coordinates for all genres being visualised
+- A file containing the 3D coordinates for all genres being visualised
+
+# How to run
+cd /music_explorer
+Change the redirect uri to 127.0.0.1:8000 in /spotify/credentials.py before running locally
+
+## Backend
+python manage.py runserver
+
+## Frontend
+cd /frontend
+npm run build
+
+Navigate to 127.0.0.1:8000 in your browser
--- data_handling/file_generator.py
+++ data_handling/file_generator.py
@@ -1,8 +1,6 @@
-import json
 from operator import indexOf
 import os
 from types import NoneType
-
 from numpy import vectorize
 import embeddings
 import data_parser
@@ -26,77 +24,57 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 def main():
-    # print(bcolors.HEADER, "Generating similarity matrix as matrix.json...")
-    # print(bcolors.OKBLUE)
-    # similarity_matrix.generate_similarity_matrix()
+    print(bcolors.HEADER, "Generating similarity matrix as matrix.json...")
+    print(bcolors.OKBLUE)
+    similarity_matrix.generate_similarity_matrix()
 
-    # # Create new Word2Vec model - tweak parameters here
-    # print(bcolors.HEADER, "Creating Word2Vec model...")
-    # print(bcolors.OKBLUE)
+    # Train new Word2Vec model - tweak parameters here
+    print(bcolors.HEADER, "Training Word2Vec model...")
+    print(bcolors.OKBLUE)
     word2vec = embeddings.create_Word2Vec_model(sg = 0)
-    # print(bcolors.HEADER, "Saving Word2Vec model...")
-    # print(bcolors.OKBLUE)
-    # embeddings.save_word2vec_model(word2vec)
+    print(bcolors.HEADER, "Saving Word2Vec model...")
+    print(bcolors.OKBLUE)
+    embeddings.save_word2vec_model(word2vec)
 
-    # print(bcolors.HEADER, "Generating index_to_genres and vice verca...")
-    # print(bcolors.OKBLUE)
-    # embeddings.generate_genre_to_index_word2vec()
-    # embeddings.generate_index_to_genre_word2vec()
-    # word2vec = embeddings.load_word2vec_model()
-    # print(bcolors.HEADER, "Generating TSNE-model and datapoints for the graph...")
-    # print(bcolors.OKBLUE)
-    # pca_2d = embeddings.create_pca_model(vectors=word2vec.wv.vectors, n_components=2)
+    print(bcolors.HEADER, "Generating index_to_genres and vice verca...")
+    print(bcolors.OKBLUE)
+    embeddings.generate_genre_to_index_word2vec()
+    embeddings.generate_index_to_genre_word2vec()
+    
+    print(bcolors.HEADER, "Generating TSNE-model and datapoints for the graph...")
+    print(bcolors.OKBLUE)
     tsne_2d: TSNE = embeddings.create_tsne_model(word2vec.wv.vectors, n_components=2)
-    containing_rock_points = []
-    not_containing_rock_points = []
-    for index, value in enumerate(word2vec.wv.index_to_key):
-        if "house" in value:
-            containing_rock_points.append(tsne_2d[index])
-        else:
-            not_containing_rock_points.append(tsne_2d[index])
-    plt.scatter(np.array(not_containing_rock_points)[:, 0], np.array(not_containing_rock_points)[:, 1], color='blue')
-    plt.scatter(np.array(containing_rock_points)[:, 0], np.array(containing_rock_points)[:, 1], color='orange')
-    plt.legend(["All other genres", "Genres containing 'House'"])
-    # plt.scatter(tsne_2d[:, 0], tsne_2d[:, 1])
-
-
-    plt.show()
-    # tsne_3d = embeddings.create_tsne_model(word2vec.wv.vectors, n_components=3)
+    tsne_3d = embeddings.create_tsne_model(word2vec.wv.vectors, n_components=3)
 
     print(bcolors.OKGREEN, "... for 2D ...")
-    # print(bcolors.OKBLUE)
-    # embeddings.generate_datapoints(pca_2d, n_components=2)
-    # embeddings.generate_datapoints(tsne_2d, n_components=2)
-    # print(bcolors.OKGREEN, "... for 3D ...")
-    # print(bcolors.OKBLUE)
-    # embeddings.generate_datapoints(tsne_3d, n_components=3)
+    print(bcolors.OKBLUE)
+    embeddings.generate_datapoints(tsne_2d, n_components=2)
+    print(bcolors.OKGREEN, "... for 3D ...")
+    print(bcolors.OKBLUE)
+    embeddings.generate_datapoints(tsne_3d, n_components=3)
 
-    # print(bcolors.HEADER,"Generating graph data file from TSNE-datapoints...")
-    # print(bcolors.OKBLUE)
-    # print(bcolors.OKGREEN, "... for 2D ...")
-    # print(bcolors.OKBLUE)
-    # data_parser.generate_graph_data_2d()
-    # print(bcolors.OKGREEN, "... for 3D ...")
-    # print(bcolors.OKBLUE)
-    # data_parser.generate_graph_data_3d()
-
-    # print(bcolors.HEADER,"Generating vector_space_graph.pkl...")
-    # print(bcolors.OKBLUE)
-    # embeddings.generate_vector_space_graph(word2vec_model=embeddings.load_word2vec_model())
+    print(bcolors.HEADER,"Generating graph data file from TSNE-datapoints...")
+    print(bcolors.OKBLUE)
+    print(bcolors.OKGREEN, "... for 2D ...")
+    print(bcolors.OKBLUE)
+    data_parser.generate_graph_data_2d()
+    print(bcolors.OKGREEN, "... for 3D ...")
+    print(bcolors.OKBLUE)
+    data_parser.generate_graph_data_3d()
 
     # TWEAK N_EDGES HERE
-    # OPTIMAL_N_EDGES = 6
-    # print(bcolors.HEADER,"Generating NetworkX graph from vector space with ... ", OPTIMAL_N_EDGES, " from each genre...")
-    # print(bcolors.OKBLUE)
-    # G = graph_util.generate_networkx_graph_from_vector_space(n_edges=OPTIMAL_N_EDGES)
+    OPTIMAL_N_EDGES = 6
+    print(bcolors.HEADER,"Generating NetworkX graph from vector space with ... ", OPTIMAL_N_EDGES, " from each genre...")
+    print(bcolors.OKBLUE)
+    G = graph_util.generate_networkx_graph_from_vector_space(n_edges=OPTIMAL_N_EDGES)
 
-    # print(bcolors.HEADER,"Saving NetworkX graph to .gml file...")
-    # print(bcolors.OKBLUE)
-    # graph_util.save_graph_as_gml(G)
+    print(bcolors.HEADER,"Saving NetworkX graph to .gml file...")
+    print(bcolors.OKBLUE)
+    graph_util.save_graph_as_gml(G)
 
-    # print(bcolors.HEADER,"Generating genre_tracks.json...")
-    # print(bcolors.OKBLUE)
-    # spotify_scraper.nomralize_genre_playlists_and_save_to_json()
+    print(bcolors.HEADER,"Generating genre_tracks.json...")
+    print(bcolors.OKBLUE)
+    spotify_scraper.nomralize_genre_playlists_and_save_to_json()
   
 main()
 
